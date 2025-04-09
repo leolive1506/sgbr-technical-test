34212e871104a6fabc1aa0b95b841493705ec8d3
Leonardo Lopes <leonardolivelopes2@gmail.com>
2025-04-09 17:08:35 -0300

    feat: validate unique slug

--- app/Http/Requests/Location/UpdateLocationRequest.php
+++ app/Http/Requests/Location/UpdateLocationRequest.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Requests\Location;
 
+use App\Models\Location;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateLocationRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:150'],
+            'name' => ['required', 'string', 'max:150', Rule::unique(Location::class, 'name')->ignore($this->location)],
             'state' => ['required', 'string', 'max:255'],
             'city' => ['required', 'string', 'max:255'],
         ];
--- database/migrations/2025_04_09_170147_create_locations_table.php
+++ database/migrations/2025_04_09_170147_create_locations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
             $table->string('name', 150);
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->string('state');
             $table->string('city');
             $table->softDeletes();
